76d9711f49e2b08e8599019a4550ef8ee9c062dc
westernwells <rajiorazaq@gmail.com>
2020-05-22 16:13:27 +0100

    route fixed

--- app.js
+++ app.js
@@ -1,61 +1,80 @@
 const express = require('express');
-const bodyparser = require('body-parser')
 const path = require('path');
-const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const app = express()
 require('dotenv/config')
-const app = express();
-const stackPosts = []
+const connectDB = require("./db")
+connectDB();
+// setting template engine
+app.set('view engine', 'ejs');
 
+
+// configuring the body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}));
+const mongoose = require('mongoose')
 const Post = require('./model/posts')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyparser.urlencoded({
-    extended: true
-}));
-app.use(bodyparser.json());
-
+// Setting up static folder up
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', ( req, res, next) => {
+// index route
+app.get('/', ( req, res) => {
     res.render('index')
 });
-app.post('/index', (res, req) => {
-    const fullName = req.body.fullname;
-    const name = req.body.name;
-    const post = {
-        fullname,
-        name
-    }
-    const conData = JSON.stringify(post);
-    console.log(conData)
+// index route
+app.get('/index', ( req, res) => {
+        res.render('index')
+      });
 
-    if (fullName === '' || mail === '') {
+// success route to render success page
+app.get('/success', ( req, res) => {
+    res.render('success')
+});
+
+// post route for the for form input
+app.post('/index', async (req, res) => {
+    const fullname = req.body.fullname;
+    const mail = req.body.mail;
+    if (fullname === '' || mail === '') {
         res.render('index', {
             Message: 'Please Input The Neccasary Value Below!!'
         })
     }
-    res.redirect('/success')
+    const postData = {
+        fullname,
+        mail
+    }
+    try {
+        const post = new Post(postData)
+        // runing out variable name lols
+       const food = await post.save()
+       console.log(food)
+        res.render('success')
+        
+    } catch (err) {
+        console.error(err.message);
+    }
+    
+});
+
+app.get('/admin', async (req, res) => {
+    const data = await Post.find()
+
+    console.log(data)
+  
+    res.render('admin', {
+        pageTitle: 'AdminPage',
+        data:data
+    })
 })
 
-app.get('/', (req, res) => {
-    res.render('/admin', {
-        pageTilte: 'AdminPage',
-        NamePost: fullname,
-        NameMail: mail
-    })
-})
-mongoose.connect(
-    process.env.DB_CONNECTION,
-    { useNewUrlParser: true,
-    useUnifiedTopolopy: true},
-    () => {
-        console.log('Connected to MongoDB!')
-    })
 
 app.use('/404-page', (req, res) => {
-    res.status(404).render('/404.page')
+    res.status(404).render('404-page')
 })
 
 app.listen(5500, () => {
