6a766f23ef132421d4213367e9e85633a158058e
Tolu Smith Jr <smyxbrone@gmail.com>
2020-06-02 07:49:46 -0700

    New Commit

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ connectDB();
 app.set('view engine', 'ejs');
 // configuring the body parser
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 const mongoose = require('mongoose')
 const Post = require('./model/posts')
 
@@ -21,19 +23,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')))
 
 // index route
-app.get('/', ( req, res) => {
+app.get('/', (req, res) => {
     res.render('index')
 });
 // index route
-app.get('/index', ( req, res) => {
-        res.render('index')
-      });
+app.get('/index', (req, res) => {
+    res.render('index')
+});
 
 // success route to render success page
-app.get('/success', ( req, res) => {
+app.get('/success', (req, res) => {
     const fullname = req.body.fullname;
-    res.render('success', 
-    {fullname:fullname
+    res.render('success', {
+        fullname: fullname
     })
 });
 
@@ -52,75 +54,80 @@ app.post('/index', async (req, res) => {
         mail
     }
     try {
-       const post = new Post(postData)
-       const food = await post.save()
+        const post = new Post(postData)
+        const food = await post.save()
         res.render('success', {
             fullname: fullname
         })
-        
+
     } catch (err) {
         console.error(err.message);
     }
-    
+
 });
 
 // app.delete('/admin', async(req,res)=>{ 
 //     const deletedPost = await Post.remove({_id: req.params.postId})
 // })
-app.post('/admin', async(req,res)=>{
+app.post('/admin', async (req, res) => {
     const allPost = await Post.find()
-var mailingList = []
-for(let i = 0; i < allPost.length; i++){
-mailingList.push( allPost[i].mail)
-}
-console.log(mailingList)
+    
+    var mailingList = []
+    for (let i = 0; i < allPost.length; i++) {
+        mailingList.push(allPost[i].mail)
+    }
+    console.log(mailingList)
     const subject = req.body.subject;
     const message = req.body.message;
-    try{
-    let testAccount = await nodemailer.createTestAccount();
-    let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        type: "OAuth2",
-        user: process.env.USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-        accessToken: process.env.ACCESS_TOKEN
-      },
-    });
+    try {
+        let testAccount = await nodemailer.createTestAccount();
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                type: "OAuth2",
+                user: process.env.USER,
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
+                accessToken: process.env.ACCESS_TOKEN
+            },
+        });
 
-    let info = await transporter.sendMail({
-      from: '"SneakersPlug NG " <ritchiehouseofclothing97@gmail.com>',
-      to: `${mailingList}`, 
-      subject:`${subject}`,
-      text: `${message}`, 
-      html: `<p>${message}</p>`
-    });
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    res.render('admin',{
-        Mesg: "Newsletter Sent Successfully!",
-        pageTitle:'Success'
-    })
- 
-  }catch(err){
-    console.log(err)
-}
+        let info = await transporter.sendMail({
+            from: '"SneakersPlug NG " <ritchiehouseofclothing97@gmail.com>',
+            to: `${mailingList}`,
+            subject: `${subject}`,
+            text: `${message}`,
+            html: `<p>${message}</p>`
+        });
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        res.render('admin', {
+            Mesg: "Newsletter Sent Successfully!",
+            pageTitle: 'Success'
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
 })
 app.get('/admin', async (req, res) => {
     const data = await Post.find()
-  
+
     res.render('admin', {
         pageTitle: 'AdminPage',
-        data:data
+        data: data
     })
 })
 app.use('/404-page', (req, res) => {
     res.status(404).render('404-page')
 })
-app.listen(5500, () => {
+
+const PORT = process.env.PORT || 5500;
+
+
+app.listen(PORT, () => {
     console.log('Server Started at 5500')
 })
